5edccf5e812072b87106f63ff65df7c9edfb174a
hackeru <adlkja>
2017-06-19 10:50:56 +0300

    finished!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
-public class Main {
+import javax.swing.*;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+public class Main {
+    static Repository rep;
     public static void main(String[] args) {
-        Controller c = new Controller();
+        //System.out.println("enter length of array");
+        //int n = Integer.parseInt(System.console().readLine());
+
+
+        int n = Integer.parseInt(JOptionPane.showInputDialog("enter the length of array (N)"));
+        int m = Integer.parseInt(JOptionPane.showInputDialog("enter the amount of thread (M)"));
+ /*       System.out.println("enter amount of thread");
+        int m = Integer.parseInt(System.console().readLine());*/
+
+        int[] nums =  new int[n];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            nums[i]  = rand.nextInt(10)+1;
+        }
+        rep = new Repository(nums);
+
+        ExecutorService exec = Executors.newFixedThreadPool(m);
+        for (int i = 0; i < m; i++) {
+            exec.execute(new SumThread());
+        }
+        exec.shutdown();
+        try {
+            exec.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        JOptionPane.showMessageDialog(null,rep.getFirst());
     }
 }
--- src/com/company/SumThread.java
+++ src/com/company/SumThread.java
@@ -1,23 +1,17 @@
 package com.company;
 
-import org.omg.PortableServer.THREAD_POLICY_ID;
 
-public class SumThread implements Runnable {
+
+public class SumThread extends Thread {
 
 
     @Override
     public void run() {
-        while(!Controller.rep.isSummed()) {
-            int[] two = Controller.rep.removeTwo();
+        while(!Main.rep.isSummed()) {
+            int[] two = Main.rep.removeTwo();
             Thread.yield();
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Controller.rep.enterSum(two[0] + two[1]);
+            Main.rep.enterSum(two[0] + two[1]);
             Thread.yield();
-            System.out.println(Thread.currentThread().getId());
         }
         }
 }
